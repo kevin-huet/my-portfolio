3b1c9917cbbda7cef567022ed4397874bd680fee
kevin-huet <kevin.huet2020@campus-eni.fr>
2022-09-18 23:10:36 +0200

    remove useless files

--- my-vue-app/src/components/Test.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="tab">
-    Home component
-  </div>
-</template>
--- my-vue-app/src/utils/data.js
+++
@@ -1,7 +0,0 @@
-export const pageName = {
-  HOME: 'home',
-  ABOUT: 'about',
-  CONTACT: 'contact',
-  SKILLS: 'skills',
-  TIMELINE: 'timeline'
-}
