aa45145b179255a7599f1b97f15492128ee88b21
kevin-huetsii <kevin.huet@sii.fr>
2025-03-07 16:33:04 +0100

    add status route

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,11 +1,15 @@
 import {Body, Controller, Get, HttpException, HttpStatus, Post} from '@nestjs/common';
 import {AppService} from './app.service';
-import {ContactMessage, Project} from "./app.type";
-import {Throttle} from "@nestjs/throttler";
+
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {
 
+  }
+
+  @Get('/status')
+  appStatus() {
+    return { message: 'OK' };
   }
 }
